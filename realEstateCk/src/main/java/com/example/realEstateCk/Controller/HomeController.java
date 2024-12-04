6a38fe735c8a269845e669ad4605b98e4efb2c99
Giahao <Admin@LAPTOP-1JGLQK4M>
2024-12-04 20:49:28 +0700

    giahao

--- realEstateCk/src/main/java/com/example/realEstateCk/Controller/HomeController.java
+++ realEstateCk/src/main/java/com/example/realEstateCk/Controller/HomeController.java
@@ -4,9 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("/a")
+public class    HomeController {
+    @GetMapping("/")
     public String login() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/agent")
+    public String agentPage() {
+        return "agent";
+    }
+
+    @GetMapping("/services")
+    public String servicesPage() {
+        return "services";
+    }
+
+    @GetMapping("/properties")
+    public String propertiesPage() {
+        return "properties";
+    }
+
+    @GetMapping("/blog")
+    public String blogPage() {
+        return "blog";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
     }
 }
